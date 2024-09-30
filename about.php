157743645d0aa24fba3145c3b688f671516bfcb4
Sandra <sandra0924@protonmail.com>
2024-09-30 16:47:34 +0200

    added html, a title and some paragraphs of text to about.php

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php require __DIR__ . '/header.php'; ?>
+
+<main>
+    <article>
+        <h2>About</h2>
+        <p>Hej, jag heter Sandra. Jag tycker inte så värst mycket om fotboll, men min pappa gillar det så jag har sett en del matcher både live och på tv. Mina bästa fotbollsminnen är från när jag var yngre, då jag tittade på när min lillebror spelade matcher eller hade träning. Mina värsta fotbollsminnen är samtliga från idrottslektioner i högstadiet, då det kändes som att både bollar och tacklingar kom flygande från alla möjliga håll.</p>
+        <p>Istället för att titta på fotboll ser jag hellre en film eller tv-serie. Genre beror på humör, men oftast känner jag för något mörkt och mystiskt. Andra dagar tycker jag om att se på något tecknat eller animerat. Dagar då allt känns "blä" brukar det bli något förutsägbart, såsom en romantisk komedi som man från början vet kommer att sluta lyckligt.</p>
+        <p>Om jag fick välja att göra en annan idrott lika populär som fotboll skulle det vara simning. Jag tycker om att simma och var som en fisk när jag var liten. Det är dessutom en livsviktig färdighet att ha, särskilt om man bor nära vatten.</p>
+    </article>
+</main>
+
+<?php require __DIR__ . '/footer.php'; ?>
